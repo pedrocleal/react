ba14ceb418ed7a84ebfb0f894b02aad88da7c848
pedrocleal <pedroxleal10@gmail.com>
2021-10-13 16:56:57 -0300

    created add new post and clear posts features

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 import Post from './Post';
 
-const posts = [
+const initialPosts = [
   {
     id: 1,
-    title: 'PRIMEIRO POST',
-    desc: 'Descrição do primeiro post',
+    title: '1º POST',
+    desc: 'Descrição do 1º post',
     likes: 3
   },
   {
     id: 2,
-    title: 'SEGUNDO POST',
-    desc: 'Descrição do segundo post',
+    title: '2º POST',
+    desc: 'Descrição do 2º post',
     likes: 2
   },
   {
     id: 3,
-    title: 'TERCEIRO POST',
-    desc: 'Descrição do terceiro post',
+    title: '3º POST',
+    desc: 'Descrição do 3º post',
     likes: 1
   },
 ]
 
 function App() {
+
+  const [ posts, setPosts ] = useState(initialPosts);
+
+  function handleAdd() {
+    // Para usar o prevState e evitar bugs na interface, é necessário executar uma função anonima na chamada de setState
+    setPosts((prevState) => [ 
+      ...prevState, //spread para capturar os dados já existentes no array
+      {
+        id: posts.length + 1,
+        title: `${posts.length + 1}º POST`,
+        desc: `Descrição do ${posts.length + 1}º POST`,
+        likes: `${posts.length + Math.random()}`
+      }
+    ])
+  }
+
+  function handleClear() {
+    setPosts([]);
+  }
+
   return (
     <>
-      <Header />
+      <Header> 
+        <button onClick={handleAdd}>Adicionar novo post</button>
+        <button onClick={handleClear}>Limpar</button>
+      </Header>
 
       <hr/>
 
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Header({ children }) {
+function Header({children}) {
   return (
     <>
       <h1>JSTACK BLOG</h1>
       <h3>Posts da semana</h3>
-
+      {children}
     </>
   )
 }
